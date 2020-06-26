d015365ffdd15d6b6b43002f8686c989c42c8526
Catherine Morie <catherine.morie@wustl.edu>
2020-06-26 13:23:24 -0500

    add prod build folder

---
+++ build/precache-manifest.aa5734e48c940d00e01e61fc938ec9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf5a7d924dd6045f79704b2487f3505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c628dd7bee4a46ac4033",
+    "url": "/static/css/main.eb9224a5.chunk.css"
+  },
+  {
+    "revision": "53596d16636225674136",
+    "url": "/static/js/2.98cc960e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.98cc960e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c628dd7bee4a46ac4033",
+    "url": "/static/js/main.7dadb5a6.chunk.js"
+  },
+  {
+    "revision": "4c79c7fa81b21e674f40",
+    "url": "/static/js/runtime-main.4ebd4da6.js"
+  }
+]);
