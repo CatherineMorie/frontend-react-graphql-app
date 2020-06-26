7fa75012fa33d2a9d0d8c375f2ee9f7be832f42d
Catherine Morie <catherine.morie@wustl.edu>
2020-06-26 10:34:26 -0500

    remove current ./build and begin Apollo setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonContainer } from './containers/PokemonContainer';
 import React from 'react';
 
 export function App() {
-  return <div>Hello there</div>;
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh',
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
+  );
 }
